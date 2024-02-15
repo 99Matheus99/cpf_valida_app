996118efb32d4b00904c79dd3144b4bcb5bec00c
matheus de oliveira <nubinhopro5@gmail.com>
2024-02-15 17:02:45 -0300

    adicionando a função que valida o cpf

--- src/cpf_valida/app.py
+++ src/cpf_valida/app.py
@@ -4,6 +4,7 @@ My first application
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import re
 
 
 class cpf_valida(toga.App):
@@ -31,8 +32,33 @@ class cpf_valida(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
+
     def valida_cpf(self, widget): # função que valida o cpf (função "handler")
-        print(f'vc digitou {self.name_input.value}')
+        # PRIMEIRO DIGITO VERIFICADOR
+        soma = digito1 = 0
+        for n in range(0,9):
+            soma += int(self.name_input.value[n]) * (10 - n)
+        resto = soma % 11
+        if(resto < 2):
+            digito1 = 0
+        else:
+            digito1 = 11 - resto
+
+        # SEGUNDO DIGITO VERIFICADOR
+        soma = digito2 = 0
+        for n in range(0,10):
+            soma += int(self.name_input.value[n]) * (11 - n)
+        resto = soma % 11
+        if(resto < 2):
+            digito2 = 0
+        else:
+            digito2 = 11 - resto
+
+        # PARTE DA ANÁLISE
+        if digito1 == int(self.name_input.value[9]) and  digito2 == int(self.name_input.value[10]):
+            return True
+        else:
+            return False
 
 def main(): 
     return cpf_valida()
